6a8c163e4389a14c7ee8b21f7ba4c4216acc60bd
jyotiraditya <jyotiraditya24@gmail.com>
2024-04-07 17:37:29 +0530

    Lazy and suspense added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
+
 function App() {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <Router>
+      <Suspense fallback={}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
