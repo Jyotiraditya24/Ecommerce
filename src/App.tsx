0f47f6ffb85aaf34572619692efcb484f43dde44
jyotiraditya <jyotiraditya24@gmail.com>
2024-04-07 17:38:50 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import Loader from "./component/loader";
 
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
@@ -8,7 +9,7 @@ const Cart = lazy(() => import("./pages/Cart"));
 function App() {
   return (
     <Router>
-      <Suspense fallback={}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/search" element={<Search />}></Route>
