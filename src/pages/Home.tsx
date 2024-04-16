a1d744ba6b6815dfced91c8b36354c339c65a0f3
jyotiraditya <jyotiraditya24@gmail.com>
2024-04-16 23:55:00 +0530

    making home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      {/* FOR MAIN IMAGE */}
+      <section>
+
+      </section>
+
+      {/* HEADER */}
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+      {/* PRODUCT CART */}
+      <main>
+
+      </main>
+    </div>
+  );
 };
 export default Home;
